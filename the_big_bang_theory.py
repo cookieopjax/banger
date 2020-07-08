525090d89e3c8fea98bc315584386eb29f84be95
Jiaxuan <mike200177@hotmail.com>
2020-07-08 17:14:47 +0800

    modify on the big bang theory

--- the_big_bang_theory.py
+++ the_big_bang_theory.py
@@ -1,1 +1,4 @@
-print('Our whole universe was in a hot, dense state')
+# here we can print the lyric of the big bang theory
+print('the theme song')
+print('it sucks')
+print('Then, 14 billion years ago expansion started, wait!')
